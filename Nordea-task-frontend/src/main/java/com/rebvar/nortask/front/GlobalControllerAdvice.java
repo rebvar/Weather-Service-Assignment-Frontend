df21a8119dd9019652459788f96d662372d82a7e
sehossei <sehossei@OY201811066.univ.yo.oulu.fi>
2019-04-18 09:39:17 +0300

    Cleared the cookie on application restart.

--- Nordea-task-frontend/src/main/java/com/rebvar/nortask/front/GlobalControllerAdvice.java
+++ Nordea-task-frontend/src/main/java/com/rebvar/nortask/front/GlobalControllerAdvice.java
@@ -1,5 +1,8 @@
 package com.rebvar.nortask.front;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
@@ -16,7 +19,7 @@ public class GlobalControllerAdvice {
 	UserService userService;
 	
     @ModelAttribute
-    public void populateUser(@CookieValue(value=AppConstants.HEADER_STRING, defaultValue = "") String authCookie, Model model) {
+    public void populateUser(@CookieValue(value=AppConstants.HEADER_STRING, defaultValue = "") String authCookie, Model model, HttpServletResponse response) {
         
     	if (authCookie.isEmpty())
     	{
@@ -30,6 +33,10 @@ public class GlobalControllerAdvice {
     		if (user==null || !user.has("userId") || user.getString("userId").length()<=0)
     		{
         		model.addAttribute("user", null);
+        		Cookie cookie = new Cookie(AppConstants.HEADER_STRING,null);
+        		cookie.setPath("/");
+        		cookie.setMaxAge(0);
+        		response.addCookie(cookie);
         		return;
         	}
     		else
@@ -41,6 +48,10 @@ public class GlobalControllerAdvice {
     	catch(Exception ex)
     	{
     		model.addAttribute("user", null);
+    		Cookie cookie = new Cookie(AppConstants.HEADER_STRING,null);
+    		cookie.setPath("/");
+    		cookie.setMaxAge(0);
+    		response.addCookie(cookie);
     		return;
     	}
     }
